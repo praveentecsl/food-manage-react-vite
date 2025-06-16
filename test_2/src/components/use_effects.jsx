f6aab1686ec08e13dbab30002586b250de530dcb
PraveenWE <praveentecsl@gmail.com>
2025-06-16 10:20:04 +0530

    create a useEffect and test1

--- test_2/src/components/use_effects.jsx
+++ test_2/src/components/use_effects.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 
 const Use_effects = () => {
+
+  const [val,setval] = useState(true)
+
+  useEffect(()=>{
+    console.log("I am a useEffect function }",val);
+
+    return()=>{
+      console.log("return function of the useEffect");
+    }
+
+  })
+
+
+
+
+
   return (
 
+    <>
+    <div>use_effectsp</div>
+    <button onClick={()=>{setval(!val)}}>Click to change the state</button>
+    <div>this is the val : {val.toString()}</div>
     
-    <div>use_effects</div>
+    </>
+    
   )
 }
 
