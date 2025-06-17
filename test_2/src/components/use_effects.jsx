63188b5ebe9cc92860a5ede741082f59a772ce2c
PraveenWE <praveentecsl@gmail.com>
2025-06-17 10:10:08 +0530

    initialize use

--- test_2/src/components/use_effects.jsx
+++ test_2/src/components/use_effects.jsx
@@ -3,15 +3,16 @@ import React,{useState,useEffect} from 'react'
 const Use_effects = () => {
 
   const [val,setval] = useState(true)
+  const [val_1,setval_1] = useState(false)
 
   useEffect(()=>{
-    console.log("I am a useEffect function }",val);
+    console.log("I am a useEffect function :",val_1);
 
     return()=>{
       console.log("return function of the useEffect");
     }
 
-  })
+  },[val_1,val])
 
 
 
@@ -23,6 +24,8 @@ const Use_effects = () => {
     <div>use_effectsp</div>
     <button onClick={()=>{setval(!val)}}>Click to change the state</button>
     <div>this is the val : {val.toString()}</div>
+    <h1>hellllllooooooo</h1>
+    <button onClick={()=>{setval_1(!val_1)}}>change the val_1 : {val_1.toString()}</button>
     
     </>
     
