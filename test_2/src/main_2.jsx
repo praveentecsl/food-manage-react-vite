24605a7bfeb68dfc9ac9b83e1b175f08e0de2703
PraveenWE <praveentecsl@gmail.com>
2025-06-17 19:49:28 +0530

    resolve the problem in commiting phase

--- test_2/src/main_2.jsx
+++ test_2/src/main_2.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import { BrowserRouter} from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import ClassComponents from './components/ClassComponents.jsx'
@@ -8,19 +8,12 @@ import FunctionalComponents from './components/FunctionalComponents.jsx'
 import Use_effects from './components/use_effects.jsx'
 import Use_memo from './components/use_Memo.jsx'
 import Use_callback from './components/use_callback.jsx'
-//import Children from './components/children.jsx'
-//import Pareent from './components/parent.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter >
-      <Routes>
-          <Route path="praveen" element={<Use_effects/>}/>
-          <Route path="chandeepa" element={<Use_memo/>}/>
-          <Route path= "weera" element= {<Pareent/>}/>
-
-
-      </Routes>
+      <Use_callback/>
       
         
     </BrowserRouter>
